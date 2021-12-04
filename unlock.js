2d087e59efaa42aed17e3ce68af7712f919cfae0
Aleks <aleks@axdrv.ru>
2021-12-04 13:39:51 +0300

    work
    Checked on mobile and desktop.

---
+++ unlock.js
@@ -0,0 +1,52 @@
+const test = document.getElementById('test')
+const slider = document.getElementById('slider');
+const bar = document.getElementById('slider-bar');
+const verifiedMsg = document.getElementById('verified-msg');
+slider.classList.add('initial');
+verifiedMsg.classList.remove('verified'); 
+let sliderWidth = slider.offsetWidth;
+let barWidth = bar.offsetWidth;
+let verified = false;
+slider.addEventListener('mousedown', function(event) {
+    event.preventDefault();
+    slider.classList.remove('initial');
+    document.addEventListener('mousemove', moveSlider);
+    document.addEventListener('mouseup', initSlider);
+});
+slider.addEventListener('touchstart', function(event) {
+    event.preventDefault();
+    console.log(event.changedTouches[0].pageX);
+    slider.classList.remove('initial');
+    document.addEventListener('touchmove', moveSlider);
+    document.addEventListener('touchend', initSlider);
+});
+function moveSlider (event) {
+    let barLeft = bar.getBoundingClientRect().left;
+    let barRight = bar.getBoundingClientRect().right;        
+    let sliderMin = slider.getBoundingClientRect().left;
+    let sliderMax = slider.getBoundingClientRect().right;
+    let sliderX = (sliderMax-sliderMin)/10;
+    let eventPos = event.pageX-barLeft || event.changedTouches[0].pageX-barLeft; 
+    let marge = eventPos-sliderMin;
+    slider.style.marginLeft = (eventPos - sliderX) + 'px';
+    if (sliderMin-barLeft < -5) { initSlider()}
+    else if (barRight - sliderMax <=5) {
+        document.removeEventListener('mousemove', moveSlider);
+        document.removeEventListener('touchmove', moveSlider);
+        document.removeEventListener('mouseup', initSlider);
+        document.removeEventListener('touchend', initSlider);
+        slider.style.marginLeft = barRight +'px';
+        bar.classList.add('verified');
+        slider.classList.add('verified');
+        verifiedMsg.classList.add('verified'); 
+        verified = true;
+    }
+}
+function initSlider () {    
+    document.removeEventListener('mousemove', moveSlider);
+    document.removeEventListener('touchmove', moveSlider);
+    x = slider.style.marginLeft = 0 +'px';
+    slider.classList.add('initial');
+}
+
+
