362a6db066ff20aeb24c53c35d7671e4e778fb7c
Aleks <aleks@axdrv.ru>
2021-12-04 17:31:11 +0300

    initSlider add control
    More control in initSlider, if work through from external element

--- unlock.js
+++ unlock.js
@@ -1,9 +1,8 @@
-const test = document.getElementById('test')
 const slider = document.getElementById('slider');
 const bar = document.getElementById('slider-bar');
 const verifiedMsg = document.getElementById('verified-msg');
 slider.classList.add('initial');
-verifiedMsg.classList.remove('verified'); 
+verifiedMsg.classList.remove('verified');
 let sliderWidth = slider.offsetWidth;
 let barWidth = bar.offsetWidth;
 let verified = false;
@@ -15,38 +14,41 @@ slider.addEventListener('mousedown', function(event) {
 });
 slider.addEventListener('touchstart', function(event) {
     event.preventDefault();
-    console.log(event.changedTouches[0].pageX);
     slider.classList.remove('initial');
     document.addEventListener('touchmove', moveSlider);
     document.addEventListener('touchend', initSlider);
 });
-function moveSlider (event) {
+
+function moveSlider(event) {
     let barLeft = bar.getBoundingClientRect().left;
-    let barRight = bar.getBoundingClientRect().right;        
+    let barRight = bar.getBoundingClientRect().right;
     let sliderMin = slider.getBoundingClientRect().left;
     let sliderMax = slider.getBoundingClientRect().right;
-    let sliderX = (sliderMax-sliderMin)/10;
-    let eventPos = event.pageX-barLeft || event.changedTouches[0].pageX-barLeft; 
-    let marge = eventPos-sliderMin;
+    let sliderX = (sliderMax - sliderMin) / 10;
+    let eventPos = event.pageX - barLeft || event.changedTouches[0].pageX - barLeft;
+    let marge = eventPos - sliderMin;
     slider.style.marginLeft = (eventPos - sliderX) + 'px';
-    if (sliderMin-barLeft < -5) { initSlider()}
-    else if (barRight - sliderMax <=5) {
+    if (sliderMin - barLeft < -5) {
+        initSlider();
+    } else if (barRight - sliderMax <= 5) {
         document.removeEventListener('mousemove', moveSlider);
         document.removeEventListener('touchmove', moveSlider);
         document.removeEventListener('mouseup', initSlider);
         document.removeEventListener('touchend', initSlider);
-        slider.style.marginLeft = barRight +'px';
+        slider.style.marginLeft = barRight + 'px';
         bar.classList.add('verified');
         slider.classList.add('verified');
-        verifiedMsg.classList.add('verified'); 
+        verifiedMsg.classList.add('verified');
         verified = true;
     }
 }
-function initSlider () {    
+
+function initSlider() {
+    dslider.style.marginLeft = 0 + 'px';
+    slider.classList.add('initial');
+    slider.classList.remove('verified');
+    verifiedMsg.classList.remove('verified');
+    bar.classList.remove('verified');
     document.removeEventListener('mousemove', moveSlider);
     document.removeEventListener('touchmove', moveSlider);
-    x = slider.style.marginLeft = 0 +'px';
-    slider.classList.add('initial');
 }
-
-
